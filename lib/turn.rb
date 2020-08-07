7e75a4d82977a335c4e1bf871d395eed922124d3
Rosario Holsen <rholsen@gmail.com>
2020-08-07 22:19:32 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+def display_board(board)
+  puts " " + board[0] + " | " + board[1] + " | " + board[2] + " "
+  puts "-----------"
+  puts " " + board[3] + " | " + board[4] + " | " + board[5] + " "
+  puts "-----------" 
+  puts " " + board[6] + " | " + board[7] + " | " + board[8] + " "
+  board = [" "," "," "," "," "," "," "," "," "]
+end
+
+def input_to_index(input)
+  input.to_i - 1 
+end 
+
+def move(index, board, player = "X")
+  board[index] = player
+end 
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == false 
+    return true 
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else index == " " || index == ""
+  end 
+end
