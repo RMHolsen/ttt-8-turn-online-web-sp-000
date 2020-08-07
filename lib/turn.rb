d6c3aaaf24d3e4971fca7caed04c7bb892b9ab56
Rosario Holsen <rholsen@gmail.com>
2020-08-07 23:39:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,10 +11,12 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index) == true
+  while valid_move?(board, index) == true
     move(board, index, player = "X")
   else 
     puts "That is not a valid move. Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
   end
   display_board(board)
 end
